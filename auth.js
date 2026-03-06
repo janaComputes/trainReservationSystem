418c464555c97e03046078f63c0773b694e756a6
Fatimah <temafa43@gmail.com>
2026-03-06 04:23:33 +0300

    staff

--- auth.js
+++ auth.js
@@ -11,11 +11,9 @@ u.username === username && u.password === password
 
 if(user){
 localStorage.setItem("currentUser", JSON.stringify(user));
-if (user.role==="ADMIN") {
+
     window.location.href="admin.html";
-} else {
-    window.location.href = "staff.html";
-}
+ 
 
 }else{
 document.getElementById("error").textContent = "Invalid login credentials";
--- dashboard.js
+++ dashboard.js
@@ -1,15 +1,30 @@
 
 const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
+// if(!currentUser){
+// window.location.href = "login.html";
+// }
+
+
+// if(window.location.pathname.includes("admin.html") && currentUser.role !== "ADMIN"){
+//  window.location.href = "staff.html";
+
+
+
+// }
+
+
+
 if(!currentUser){
 window.location.href = "login.html";
 }
 
+if(currentUser.role === "STAFF"){
 
-if(window.location.pathname.includes("admin.html") && currentUser.role !== "ADMIN"){
-window.location.href = "staff.html";
+document.getElementById("addTrain").style.display = "none";
+document.getElementById("addPassenger").style.display = "none";
+
 }
-
 function logout(){
 localStorage.removeItem("currentUser");
 window.location.href = "login.html";
